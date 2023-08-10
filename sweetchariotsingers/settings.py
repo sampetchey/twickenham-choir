f9d05e3a532949b8b5414504466493a6148eeb7a
sampetchey <sam_petchey@yahoo.com>
2023-08-10 13:59:29 +0100

    add and setup payments and views

--- sweetchariotsingers/settings.py
+++ sweetchariotsingers/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'payments.apps.PaymentsConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "sweetchariotsingers.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
